b200701ecf8eb0ecc471a6add52f2f26843624a2
cashalotkzn <cashalotkzn@gmail.com>
2021-10-21 04:37:30 +0300

    Add alwaysEnabled props to button component

--- src/components/@ui/Button.svelte
+++ src/components/@ui/Button.svelte
@@ -2,12 +2,14 @@
     export let text!: string
     export let loading = false
     export let disabled = false
+    export let alwaysEnabled = false
 </script>
 
 <div class="button__wrapper">
   <button on:click
           class="btn btn-lg glass"
           class:loading={ loading }
+          class:btn_enabled={ alwaysEnabled }
           { disabled }>{ text }</button>
 </div>
 
@@ -18,6 +20,10 @@
       &__wrapper {
         & :global {
           .btn {
+            &_enabled {
+              pointer-events: all;
+            }
+
             &.loading {
               &::before {
                 margin-right: 1.5rem;
