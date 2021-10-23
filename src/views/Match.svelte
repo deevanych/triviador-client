3c9d534dc031c4cb736671c5700ad29a4c3ad6ec
cashalotkzn <cashalotkzn@gmail.com>
2021-10-23 03:11:46 +0300

    Add players list in match view

--- src/views/Match.svelte
+++ src/views/Match.svelte
@@ -2,13 +2,51 @@
   import Map from '../components/Map.svelte'
   import { isLoading } from '../store'
   import socket from '../plugins/socket.io'
+  import { onMount } from 'svelte'
 
-  isLoading.set(false)
-  socket.emit('getMatchData')
+  let match
+  let show = false
+
+  onMount(() => {
+    isLoading.set(false)
+    socket.emit('getMatchData')
+
+    socket.on('matchData', (data) => {
+      match = data
+      show = true
+    })
+  })
 </script>
 
+{#if show}
+<div class="player__list">
+  {#each match.getUsers as player}
+  <div class="player__item">
+    <div class="avatar player__avatar">
+      <div class="rounded-full w-10 h-10">
+        <img src={ player.avatar_url }>
+      </div>
+    </div>
+    <div class="player__nickname">{ player.nickname }</div>
+  </div>
+  {/each}
+</div>
+{/if}
 <Map/>
 
 <style lang="scss">
+  .player {
+    &__list {
+      display: flex;
+      gap: 1rem;
+      flex-direction: column;
+      position: absolute;
+    }
 
+    &__item {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+    }
+  }
 </style>
