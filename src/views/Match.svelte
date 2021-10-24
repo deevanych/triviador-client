9d68632e02218e1cb887cbf2abc6328984475e52
cashalotkzn <cashalotkzn@gmail.com>
2021-10-24 22:58:58 +0300

    Add colors to avatars

--- src/views/Match.svelte
+++ src/views/Match.svelte
@@ -9,6 +9,11 @@
   let match
   let show = false
   let matchPlayers: User[] = []
+  const playersColors = [
+    'secondary',
+    'accent',
+    'error',
+  ]
 
   onMount(() => {
     socket.emit('getMatchData')
@@ -48,14 +53,20 @@
 
 {#if show}
 <div class="player__list">
-  {#each matchPlayers as player}
+  {#each matchPlayers as player, index}
   <div class="player__item">
-    <div class="avatar player__avatar" class:player__item_disconnected={ player.disconnected === true }>
-      <div class="rounded-full w-10 h-10">
+    <div class:avatar={ true }
+         class:player__item_disconnected={ player.disconnected === true }
+        >
+      <div class={`ring-${playersColors[index]}`}
+           class:player__avatar={ true }
+           class:ring={ true }>
         <img src={ player.avatar_url } alt={ player.nickname }/>
       </div>
     </div>
-    <div class="player__nickname" class:player__item_disconnected={ player.disconnected === true }>
+    <div class:player__nickname={ true }
+         class:player__item_disconnected={ player.disconnected === true }
+         class={`ring-${playersColors[index]}`}>
       { player.nickname }
     </div>
     {#if player.disconnected }
@@ -68,6 +79,36 @@
 </div>
 {/if}
 <Map/>
+<div class="match__turns">
+  <div class="overflow-x-auto">
+    <ul class="w-full steps">
+      <li class="step">start</li>
+      <li class="step step-secondary">2</li>
+      <li class="step step-secondary">3</li>
+      <li class="step step-secondary">4</li>
+      <li class="step">5</li>
+      <li class="step step-accent">6</li>
+      <li class="step step-accent">7</li>
+      <li class="step">8</li>
+      <li class="step step-error">9</li>
+      <li class="step step-error">10</li>
+      <li class="step">11</li>
+      <li class="step">12</li>
+      <li class="step step-warning">13</li>
+      <li class="step step-warning">14</li>
+      <li class="step">15</li>
+      <li class="step step-neutral">16</li>
+      <li class="step step-neutral">17</li>
+      <li class="step step-neutral">18</li>
+      <li class="step step-neutral">19</li>
+      <li class="step step-neutral">20</li>
+      <li class="step step-neutral">21</li>
+      <li class="step step-neutral">22</li>
+      <li class="step step-neutral">23</li>
+      <li class="step step-neutral">end</li>
+    </ul>
+  </div>
+</div>
 
 <style lang="scss">
   @keyframes blink {
@@ -85,6 +126,12 @@
   }
 
   .player {
+    &__avatar {
+      border-radius: 50%;
+      width: 3rem;
+      height: 3rem;
+    }
+
     &__list {
       display: flex;
       gap: 1rem;
@@ -107,6 +154,14 @@
       width: 1rem;
       height: 1rem;
       animation: blink 2s infinite;
+    }
+  }
+
+  .match {
+    &__turns {
+      position: fixed;
+      left: 2rem;
+      bottom: 2rem;
     }
   }
 </style>
